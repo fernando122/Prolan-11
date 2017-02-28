b26cc0c57afcbcc9411a474d7dac6c2bb1ead492
fernando122 <sitohang97@gmail.com>
2017-02-28 19:01:04 +0700

    Add files via upload

---
+++ Tugas 2/Mahasiswa.java
@@ -0,0 +1,7 @@
+class Mahasiswa {
+	public static void main(String[] args){
+		System.out.println("Nama   : Fernando");
+		System.out.println("NIM    : 10115347");
+		System.out.println("Alamat : Melong Green Jl. Bomber 4 no 14 ");
+	}
+}
