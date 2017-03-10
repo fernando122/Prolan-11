8101c332f4296831f2d5050cb7d8eea5e858cae4
fernando122 <sitohang97@gmail.com>
2017-03-10 21:10:48 +0700

    Add files via upload

---
+++ Tugas 5/bukudata.java
@@ -0,0 +1,27 @@
+public class bukudata {
+
+	public static void main (String[] args) {
+	
+	String judul = "Malin Kundang";
+	String penerbit ="Mulia Jaya";
+	System.out.println(judul);
+	System.out.println(penerbit);
+	
+	int harga = 75000;
+	System.out.println("Harga Buku Rp:" + harga);
+	
+	byte jum_hlm = 80;
+	System.out.println("Jumlah Halaman:" + jum_hlm);
+	}
+protected void Baca () {
+		System.out.println ("Membaca Dongeng");
+	}
+protected void Tulis () {
+		System.out.println ("Menulis Kalimat");
+	}
+
+}
+	
+	
+	
+	
