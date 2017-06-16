3d26b05fa70f89593d23ad548126f7c2e6851d26
fernando122 <sitohang97@gmail.com>
2017-06-17 05:47:11 +0700

    Add files via upload

---
+++ Tugas16/toko/Penjaga.java
@@ -0,0 +1,19 @@
+package toko;
+
+public class Penjaga {
+
+	//method generic 
+    public <T> void npenjaga (T nama) {
+        System.out.println("Nama Penjaga \t:"+nama);     
+    }
+    public <T> void nomorpeg (T usia) {
+        System.out.println("Nomor Penjaga \t:"+usia);
+    }
+
+    public <T,N> void Bpenjaga (T x , N y) {
+        System.out.println("Usia\t\t:"+x);
+        System.out.println("Jenis Kelamin\t:"+y);
+    } 
+}
+
+
